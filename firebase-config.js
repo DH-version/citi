66f621980b978f6611efc78adac24503381cba43
DH-version <hassan.dani@outlook.com>
2026-06-23 06:53:44 +1000

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Qqwl0GXwObMiGOQhrWpzoPvMKrsA3So",
+  authDomain: "geelong-premium-network-f54a5.firebaseapp.com",
+  projectId: "geelong-premium-network-f54a5",
+  storageBucket: "geelong-premium-network-f54a5.firebasestorage.app",
+  messagingSenderId: "816971005813",
+  appId: "1:816971005813:web:b1c3e2c6eb4a677e475d3c",
+  measurementId: "G-X7CVVHRPLX"
+};
